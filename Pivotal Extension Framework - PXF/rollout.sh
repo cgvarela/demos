7a32d98af974679eab9263bf2c4f89f4db184199
jgronline@gmail.com <jgronline@gmail.com>
2015-09-18 10:25:56 -0500

    0.0.117

--- Pivotal Extension Framework - PXF/rollout.sh
+++ Pivotal Extension Framework - PXF/rollout.sh
@@ -8,51 +8,6 @@ WHOAMI=`whoami`
 PXF_DATA_DIR=/user/$WHOAMI/sample
 NN_PORT=50070
 
-source_bashrc()
-{
-	source ~/.bashrc
-}
-
-set_psqlrc()
-{
-	echo "\timing" > ~/.psqlrc
-	
-}
-
-log()
-{
-	#duration
-	T="$(($(date +%s%N)-T))"
-
-	id=`echo $i | awk -F '.' ' { print $1 } '`
-
-	# seconds
-	S="$((T/1000000000))"
-	# milliseconds
-	M="$((T/1000000))"
-
-	printf "$id|$table_name|%02d:%02d:%02d.%03d\n" "$((S/3600%24))" "$((S/60%60))" "$((S%60))" "${M}" >> rollout.log
-	
-}
-
-create_reports_schema()
-{
-	local schema_count=`psql -A -t -c "SELECT count(*) from pg_namespace WHERE nspname = 'reports'"`
-	if [ "$schema_count" -eq "0" ]; then
-		psql -c "CREATE SCHEMA reports;"
-	fi
-
-	local table_count=`psql -A -t -c "SELECT count(*) from pg_namespace n JOIN pg_class c ON c.relnamespace = n.oid  WHERE n.nspname = 'reports' AND c.relname = '$DEMO' AND relstorage = 'x'"`
-	if [ "$table_count" -eq "0" ]; then
-		psql -c "CREATE EXTERNAL WEB TABLE reports.$DEMO (id int, table_name varchar, duration time) EXECUTE 'cat \"$PWD/rollout.log\"' ON MASTER FORMAT 'TEXT' (DELIMITER '|');"
-	fi
-}
-
-hadoop_init()
-{
-	sudo -u hdfs hdfs dfs -rm -f -r -skipTrash $PXF_DATA_DIR
-}
-
 echo ""
 echo "############################################################################"
 echo "This is a demonstration of Pivotal HAWQ Pivotal Extenstion Frameworkd (PXF)"
@@ -90,17 +45,17 @@ echo ""
 echo "############################################################################"
 echo "Create reporting schema for later analysis of HAWQ"
 echo "############################################################################"
-create_reports_schema
+create_reports_schema $PWD
 echo ""
 echo "############################################################################"
 echo "Remove old logs"
 echo "############################################################################"
-rm -f rollout.log
+remove_old_log $PWD
 echo ""
 echo "############################################################################"
 echo "Init Hadoop"
 echo "############################################################################"
-hadoop_init
+hadoop_init $PXF_DATA_DIR
 echo ""
 # Step 1
 for i in $( ls *.step1.sql ); do
--- TPC-H Benchmark/rollout.sh
+++ TPC-H Benchmark/rollout.sh
@@ -21,82 +21,6 @@ if [ "$GPFDIST_PORT" == "" ]; then
 	exit 1
 fi
 
-source_bashrc()
-{
-	source ~/.bashrc
-}
-
-set_psqlrc()
-{
-	echo "\timing" > ~/.psqlrc
-	
-}
-
-start_gpfdist()
-{
-	local count=`ps -ef | grep gpfdist | grep $GPFDIST_PORT | grep -v grep | wc -l`
-	if [ "$count" -eq "1" ]; then
-		gpfdist_pid=`ps -ef | grep gpfdist | grep $GPFDIST_PORT | grep -v grep | awk -F ' ' '{print $2}'`
-		if [ "$gpfdist_pid" != "" ]; then
-			echo "Stopping gpfdist on pid: $gpfdist_pid"
-			kill $gpfdist_pid
-			sleep 2
-		fi
-	fi
-	echo "Starting gpfdist port $GPFDIST_PORT"
-	echo "gpfdist -d \"/$DATA_DIR/$DEMO_DIR/data\" -p $GPFDIST_PORT >> /dev/null 2>&1 < /dev/null &"
-	gpfdist -d /$DATA_DIR/$DEMO_DIR/data -p $GPFDIST_PORT >> /dev/null 2>&1 < /dev/null &
-	gpfdist_pid=$!
-
-	# check gpfdist process was started
-	if [ "$gpfdist_pid" -ne "0" ]; then
-		sleep 0.4
-		count=`ps -ef 2> /dev/null | grep -v grep | awk -F ' ' '{print $2}' | grep $gpfdist_pid | wc -l`
-		if [ "$count" -eq "1" ]; then
-			echo "gpfdist started on port $GPFDIST_PORT"
-		else
-			echo "ERROR: gpfdist couldn't start on port $GPFDIST_PORT"
-			exit 1
-		fi
-	fi
-}
-
-stop_gpfdist()
-{
-	if [ "$gpfdist_pid" != "" ]; then
-		echo "Stopping gpfdist on pid: $gpfdist_pid"
-		kill $gpfdist_pid
-	fi
-}
-
-log()
-{
-	#duration
-	T="$(($(date +%s%N)-T))"
-
-	id=`echo $i | awk -F '.' ' { print $1 } '`
-
-	# seconds
-	S="$((T/1000000000))"
-	# milliseconds
-	M="$((T/1000000))"
-
-	printf "$id|$table_name|%02d:%02d:%02d.%03d\n" "$((S/3600%24))" "$((S/60%60))" "$((S%60))" "${M}" >> rollout.log
-}
-
-create_reports_schema()
-{
-	local schema_count=`psql -A -t -c "SELECT count(*) from pg_namespace WHERE nspname = 'reports'"`
-	if [ "$schema_count" -eq "0" ]; then
-		psql -c "CREATE SCHEMA reports;"
-	fi
-
-	local table_count=`psql -A -t -c "SELECT count(*) from pg_namespace n JOIN pg_class c ON c.relnamespace = n.oid  WHERE n.nspname = 'reports' AND c.relname = '$DEMO' AND relstorage = 'x'"`
-	if [ "$table_count" -eq "0" ]; then
-		psql -c "CREATE EXTERNAL WEB TABLE reports.$DEMO (id int, table_name varchar, duration time) EXECUTE 'cat \"$PWD/rollout.log\"' ON MASTER FORMAT 'TEXT' (DELIMITER '|');"
-	fi
-}
-
 echo ""
 echo "############################################################################"
 echo "This is a demonstration of the performance and SQL compliance of Pivotal"
@@ -134,12 +58,12 @@ echo ""
 echo "############################################################################"
 echo "Create reporting schema for later analysis of HAWQ"
 echo "############################################################################"
-create_reports_schema
+create_reports_schema $PWD
 echo ""
 echo "############################################################################"
 echo "Remove old logs"
 echo "############################################################################"
-rm -f rollout.log
+remove_old_log $PWD
 echo ""
 echo "############################################################################"
 echo "HAWQ Begin"
